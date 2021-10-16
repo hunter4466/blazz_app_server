d06199377f0d7ddd90a52b6a041218daee5fd7c4
SDEV-ANDROID <75796007+SDEV-ANDROID@users.noreply.github.com>
2021-10-16 17:02:51 -0500

    Module My_business Creating forms and new style

--- src/components/modules/my_business.js
+++ src/components/modules/my_business.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 const Mybusiness = () => (
-  <div className="homepage_container">
+  <div>
     <h1>My business</h1>
+    <br />
+    <form className="form" noValidate>
+      <div className="form-inputs">
+        <input
+          className="form-input"
+          type="email"
+          name="email"
+          placeholder="Edita tu correo"
+        />
+      </div>
+      <div className="form-inputs">
+        <input
+          className="form-input"
+          type="password"
+          name="password"
+          placeholder="Edita tu contraseña"
+        />
+      </div>
+      <button type="submit">
+        Actualizar
+      </button>
+    </form>
   </div>
+
 );
 export default Mybusiness;
--- zcommands.md
+++ zcommands.md
@@ -1,2 +1,4 @@
 npx eslint . --fix
 npx stylelint "**/*.{css,scss}" --fix
+npm run start_server
+npm run start_app
